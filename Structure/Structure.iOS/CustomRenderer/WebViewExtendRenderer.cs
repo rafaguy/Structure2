3ae26efb55c74567895db31adb762e42966bc71d
Fabrice Guyau RANAMPY <fgranampy@ASTEKMTIUS>
2017-11-03 12:18:59 +0400

    commit 3/11/2017

--- Structure/Structure.iOS/CustomRenderer/WebViewExtendRenderer.cs
+++ Structure/Structure.iOS/CustomRenderer/WebViewExtendRenderer.cs
@@ -4,24 +4,44 @@ using Structure.iOS.CustomRenderer;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
 [assembly: ExportRenderer(typeof(WebViewExtended), typeof(WebViewExtendRenderer))]
 namespace Structure.iOS.CustomRenderer
 {
-   public class WebViewExtendRenderer: Xamarin.Forms.Platform.iOS.WebViewRenderer
+   public class WebViewExtendRenderer:ViewRenderer<WebViewExtended, UIWebView>
     {
-        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+
+        protected override void OnElementChanged(ElementChangedEventArgs<WebViewExtended> e)
         {
-            base.OnElementChanged(e);
-            ScalesPageToFit = true;
-            LoadRequest(new Foundation.NSUrlRequest(new NSUrl("https://appear.in/ags-paris01")));
-          
+           // base.OnElementChanged(e);
+            SetNativeControl(new UIWebView());
+            if(e.NewElement !=null)
+            {
+                SettingWebView();
+            }
         }
+        private void SettingWebView()
+        {
+           
+
+            var url = "https://appear.in/ags-paris01";
+            Control.ScalesPageToFit = true;
+            Control.LoadRequest(new NSUrlRequest(new NSUrl(url)));
+
+        }
+        /* protected override void OnElementChanged(VisualElementChangedEventArgs e)
+         {
+             base.OnElementChanged(e);
+             ScalesPageToFit = true;
+             LoadRequest(new Foundation.NSUrlRequest(new NSUrl("https://appear.in/ags-paris01")));
+
+         }*/
         /// <summary>
         /// Settings the web view.
         /// </summary>
-      
+
     }
 }
